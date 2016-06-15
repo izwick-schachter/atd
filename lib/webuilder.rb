43f86a0ceb54b618812857dda1bb39a528a2a768
ACecretMaster <izwick.schachter@gmail.com>
2016-06-15 06:26:01 -0400

    Initial commit

---
+++ lib/webuilder.rb
@@ -0,0 +1,140 @@
+require "rack"
+require "webrick"
+
+module Webuilder
+
+	class App
+
+		##
+		# Allows Webuilder::RequestHandlers to access env["PATH_INFO"]
+
+		def self.path_info
+			@@path_info
+		end
+
+		##
+		# The real rack app
+
+		def self.call(env)
+			##
+			# Set default values for a default page (by default empty plaintext page)
+			page = []
+			headers = {}
+			status_code = 200
+			##
+			# Checks if a path doesn't exist. If it doesn't exist return 404
+			if Path.paths[env["PATH_INFO"]].nil?
+				page.push("404 Error: Path Not Found")
+				status_code = 404
+			else
+				##
+				# Otherwise
+				##
+				# 1. Sets @@path_info to env["PATH_INFO"] for use in Webuilder::RequestHandlers
+				@@path_info = env["PATH_INFO"]
+				##
+				# 2. Sets ouput to wherever Webuilder::RequestHandlers returns from RequestHandlers.get / .post call
+				output = RequestHandlers.public_send(Path.paths[env["PATH_INFO"]].method) if [:get, :post].include? Path.paths[env["PATH_INFO"]].method.downcase
+				##
+				# 3. Converts ouput to a usable rack ouput
+				headers["content-type"] = output[:"content-type"]
+				page.push(output[:content])
+			end
+			##
+			# Returns the resulting packet
+			return [status_code, headers, page]
+		end
+	end
+
+	##
+	# Manages all the initally created paths.
+	# They're all stored here, and this class is queried by the Webuilder::App.call (rack server) to check if a path exists
+	class Path
+		@@paths = {}
+
+		@headers = nil
+		@action = nil
+		@method = nil
+		@output = nil
+
+		attr_reader :headers, :action, :method, :output
+
+		def initialize(path, headers, action, method, output)
+			puts "Path #{path} initialized"
+			@headers = headers #Why...?
+			@action = action # http meth
+			@method = method # code
+			@output = output
+			@@paths[path] = self
+		end
+
+		##
+		# Returns all the paths that exist. Queried by the Webuilder::App.call (rack server)
+
+		def self.paths
+			@@paths
+		end
+	end
+
+	module RequestHandlers
+		def self.get
+			all
+			if !Path.paths[App.path_info].output.is_a?(Hash)
+				return {:"content-type" => "text/plain", :content => Path.paths[App.path_info].output}
+			else
+				return Path.paths[App.path_info].output
+			end
+		end
+
+		def self.post
+			all
+		end
+
+		def self.all
+			Path.paths[App.path_info].action.call unless Path.paths[App.path_info].action == nil
+		end
+	end
+
+	module Renderers
+		def self.html(file)
+			return {:content => File.read("./public/"+Validations.public_folder(file)), :"content-type" => "text/html"}
+		end
+	end
+
+	module Validations
+		def self.public_folder(file_name)
+			return file_name.gsub(/(.\.\.|[^a-zA-Z0-9\.\\\/\-])/,"")
+		end
+	end
+
+	##
+	# Manages the (currently only webrick) server
+	
+	module Server
+		@@server = nil
+		def self.start
+			@@server = Rack::Handler::WEBrick
+			@@server.run App
+		end
+
+		def self.stop
+			@@server.shutdown
+		end
+	end
+end
+
+##
+# The code below allows the main functions to be easily accessable from the main file.
+# Probobly can be edited later to concat them to the Main Object... Whatever that is
+
+methods=[:get,:post]
+
+methods.each do |name|
+	define_method(name) do |path, output = "Hello World!", headers = nil, &block|
+		Webuilder::Path.new(path,headers,block,name,output)
+	end
+end
+
+def html(file)
+	Webuilder::Renderers.html(file)
+end
---
+++ webuilder.gemspec
@@ -0,0 +1,15 @@
+Gem::Specification.new do |s|
+  s.name        = 'webuilder'
+  s.version     = '0.0.0'
+  s.date        = '2016-04-31'
+  s.summary     = "A gem with the simplicity of sinatra, but the scalability of rails."
+  s.description = "A gem with the simplicity of sinatra, but the scalability of rails."
+  s.authors     = ["Isaiah Zwick-Schachter"]
+  s.email       = 'izwick.schachter@gmail.com'
+  s.files       = ["lib/webuilder.rb"]
+  s.homepage    =
+    'http://rubygems.org/gems/webuilder'
+  s.license       = 'MIT'
+  s.add_runtime_dependency "rack", "~> 1.6"
+  s.add_runtime_dependency "webrick", "~> 1.3"
+end
