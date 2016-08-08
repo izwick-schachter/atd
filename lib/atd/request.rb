cdf08eb56a2ede0fd11207d02bbf249d478e9035
Isaiah Zwick-Schachter <doctorzaius1@Isaiahs-MacBook-Air.local>
2016-08-07 20:31:12 -0400

    (Issue #3) I'm moving this back to help wanted because I can't seem to make this work. Maybe someone sould try to extend Rack::Request. For now, I'm moving this issue to version 1.0.0. I will re-evaluate later.

--- lib/atd/request.rb
+++ lib/atd/request.rb
@@ -1,8 +1,21 @@
 class ATD::Request
-	attr_accessor :request, :response
+	attr_accessor :request, :response, :env
 
 	def initialize(env)
+		@env = env
 		@request = Rack::Request.new(env)
 		@response = Rack::Response.new(env)
+	end
+
+	def setrequest(newval)
+		puts "Setting ATD::Request.request to #{newval}"
+		a=Rack::Request.new(@env)
+		a.params[:test] = "reslust"
+	end
+
+	def getrequest
+		puts "Getting Request"
+		a=Rack::Request.new(@env)
+		puts a.params
 	end
 end
